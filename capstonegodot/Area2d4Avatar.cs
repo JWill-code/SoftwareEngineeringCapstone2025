73e700e899d65715a689b6cfde7a850786f330b1
JWill-code <willjg24@uwgb.edu>
2025-04-21 00:06:34 -0500

    grid system edits
    most of the grid system stuff, please try to work on this

--- capstonegodot/Area2d4Avatar.cs
+++ capstonegodot/Area2d4Avatar.cs
@@ -2,7 +2,13 @@ using Godot;
 using System;
 
 public partial class Area2d4Avatar : Area2D
+
+
 {
+	// Don't forget to rebuild the project so the editor knows about the new signal.
+
+	[Signal]
+	public delegate void HitEventHandler();
 	[Export]
 	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
 
@@ -11,6 +17,21 @@ public partial class Area2d4Avatar : Area2D
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+	// We also specified this function name in PascalCase in the editor's connection window.
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
